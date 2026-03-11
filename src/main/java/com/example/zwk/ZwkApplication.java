b58a270fbdbc4832711a08090a36d5fbd7a5d284
曾经的王 <41469832+gakkilover@users.noreply.github.com>
2026-03-11 22:14:19 +0800

    添加基础项目文件

---
+++ src/main/java/com/example/zwk/ZwkApplication.java
@@ -0,0 +1,13 @@
+package com.example.zwk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZwkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZwkApplication.class, args);
+    }
+
+}
